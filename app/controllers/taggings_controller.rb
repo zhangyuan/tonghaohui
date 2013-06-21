95e2cbdd7ffbe3f31fb462dc28974edfc3628f8b
Yuan Cheung <zhangyuan@users.noreply.github.com>
2013-06-21 17:00:52 +0800

    fix taggings_controller

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,5 +1,5 @@
 class TaggingsController < ApplicationController
   def show
-    @taggings = Tagging.published_as(:published).order('id desc').where(indexable: params[:title].to_s.downcase).includes(:post => [:user, :taggings]).page(current_page).per_page(20)
+    @taggings = Tagging.published_as(:published).order('id desc').where('taggable_id > 0').where(indexable: params[:title].to_s.downcase).includes(:post => [:user, :taggings]).page(current_page).per_page(20)
   end
 end
