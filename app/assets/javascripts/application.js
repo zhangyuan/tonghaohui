c749362d07e475958a0dd226e36f248a8e01d118
Yuan Cheung <zhangyuan@users.noreply.github.com>
2013-08-03 22:46:42 +0800

    add timeago

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.timeago
+//= require jquery.timeago.zh-CN
 //= require main
 //= require posts
--- app/assets/javascripts/main.js.coffee
+++ app/assets/javascripts/main.js.coffee
@@ -1,3 +1,4 @@
 jQuery ->
   $("a[data-click-url]").on 'click', ->
     $.get $(this).attr('data-click-url')
+  $("abbr.timeago").timeago()
